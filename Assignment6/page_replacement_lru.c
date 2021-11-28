1c4d21ec0a75cfb7146cb486d543de4bb86d8265
VaradMash <varadmash2201@gmail.com>
2021-11-28 22:57:22 +0530

    Added code for least recently used disk scheduling algorithm(demonstrative).

--- Assignment6/page_replacement_lru.c
+++ Assignment6/page_replacement_lru.c
@@ -45,15 +45,34 @@ void display_buffer(int iteration, int frame_size)
     }
 }
 
+int latest_page_location(int page, int frame_size)
+{
+
+    /*
+     *  Input   :   Page ID, frame size
+     *  Utility :   Find latest location of page in buffer.
+     *  Output  :   Location
+     */
+    int location = -1, i;
+    for(i = 0; i < frame_size; i++)
+    {
+        if(frame_buffer[i] == page)
+        {
+            location = i;
+        }
+    }
+    return location;
+}
+
 void page_replacement_algorithm(int frame_size, int input_size)
 {
     /*
      *  Input   :   Frame Buffer Size, Input size
      *  Utility :   Employ page replacement algorithm on given page sequence
-     *          :   (First In First Out)
+     *          :   (Least Recently Used)
      *  Output  :   Hit ratio and miss ratio on console.
      */
-    int hits = 0, i, tail = 0, hit_index;
+    int hits = 0, i, index = 0, hit_index, page_faults = 0;
     float hit_ratio, miss_ratio;
     for(i = 0; i < input_size; i++)
     {
@@ -68,11 +87,19 @@ void page_replacement_algorithm(int frame_size, int input_size)
         else
         {
             printf("\n\nPage not found in buffer (miss)");
-            printf("\nPage %d placed at location %d", input_string[i], tail);
-            frame_buffer[tail] = input_string[i];
-            tail = (tail + 1)%frame_size;
+            printf("\nPage %d placed at location %d", input_string[i], index);
+            
+            if (i < frame_size)
+            {
+                index = i;
+            }
+            else
+            {
+                index = latest_page_location(input_string[i - frame_size], frame_size);
+            }
+            printf("\nReplacing oldest frame in buffer at location (%d) with page %d",index, input_string[i]);
+            frame_buffer[index] = input_string[i];
         }
-        printf("\nReplacing frame at the oldest location (%d) with page %d",tail, input_string[i]);
         printf("\nContents of Frame Buffer : ");
         display_buffer(i, frame_size);
     }
@@ -117,7 +144,7 @@ int main()
     // Get input for input string
     get_input(input_size);
 
-    // Employ FIFO algorithm
+    // Employ LRU algorithm
     page_replacement_algorithm(frame_size, input_size);
 
     return 0;
@@ -126,211 +153,226 @@ int main()
 /*
     Output  :   
 
-    varadmash@varadmash-G3-3590:~/OS_Lab/Assignment6$ ./pr_lru.out 
 Page replacement algorithm : LEAST RECENT USED
 
 Enter Size of Frame Buffer : 3
 
-Enter Size of Input String : 10
+Enter Size of Input String : 19
 
 
 Enter Input Stream of Page sequences : 
-Page 0	:	4
+Page 0	:	3
 
-Page 1	:	7
+Page 1	:	2
 
-Page 2	:	6
-
-Page 3	:	1
-
-Page 4	:	7
-
-Page 5	:	6
-
-Page 6	:	1
-
-Page 7	:	2
-
-Page 8	:	7
-
-Page 9	:	2
-
-
-The given Page Sequence is as follows : 
-4 7 6 1 7 6 1 2 7 2 
-
-Page not found in buffer (miss)
-Page 4 placed at location 0
-Replacing frame at the oldest location (1) with page 4
-Contents of Frame Buffer : 
-4
-
-
-Page not found in buffer (miss)
-Page 7 placed at location 1
-Replacing frame at the oldest location (2) with page 7
-Contents of Frame Buffer : 
-7
-4
-
-
-Page not found in buffer (miss)
-Page 6 placed at location 2
-Replacing frame at the oldest location (0) with page 6
-Contents of Frame Buffer : 
-6
-7
-4
-
-
-Page not found in buffer (miss)
-Page 1 placed at location 0
-Replacing frame at the oldest location (1) with page 1
-Contents of Frame Buffer : 
-6
-7
-1
-
-
-The page 7 is found in the buffer (hit)
-
-Replacing page at index 4 in buffer 
-Replacing frame at the oldest location (1) with page 7
-Contents of Frame Buffer : 
-6
-7
-1
-
-
-The page 6 is found in the buffer (hit)
-
-Replacing page at index 5 in buffer 
-Replacing frame at the oldest location (1) with page 6
-Contents of Frame Buffer : 
-6
-7
-1
-
-
-The page 1 is found in the buffer (hit)
-
-Replacing page at index 6 in buffer 
-Replacing frame at the oldest location (1) with page 1
-Contents of Frame Buffer : 
-6
-7
-1
-
-
-Page not found in buffer (miss)
-Page 2 placed at location 1
-Replacing frame at the oldest location (2) with page 2
-Contents of Frame Buffer : 
-6
-2
-1
-
-
-Page not found in buffer (miss)
-Page 7 placed at location 2
-Replacing frame at the oldest location (0) with page 7
-Contents of Frame Buffer : 
-7
-2
-1
-
-
-The page 2 is found in the buffer (hit)
-
-Replacing page at index 9 in buffer 
-Replacing frame at the oldest location (0) with page 2
-Contents of Frame Buffer : 
-7
-2
-1
-
-
-Hit Ratio : 0.400000
-Miss Ratio: 0.600000
-varadmash@varadmash-G3-3590:~/OS_Lab/Assignment6$ ./pr_lru.out 
-Page replacement algorithm : LEAST RECENT USED
-
-Enter Size of Frame Buffer : 3
-
-Enter Size of Input String : 6
-
-
-Enter Input Stream of Page sequences : 
-Page 0	:	1
-
-Page 1	:	3
-
-Page 2	:	0
+Page 2	:	1
 
 Page 3	:	3
 
-Page 4	:	5
+Page 4	:	4
 
-Page 5	:	6
+Page 5	:	1
+
+Page 6	:	6
+
+Page 7	:	2
+
+Page 8	:	4
+
+Page 9	:	3
+
+Page 10	:	4
+
+Page 11	:	2
+
+Page 12	:	1
+
+Page 13	:	4
+
+Page 14	:	5
+
+Page 15	:	2
+
+Page 16	:	1
+
+Page 17	:	3
+
+Page 18	:	4
 
 
 The given Page Sequence is as follows : 
-1 3 0 3 5 6 
+3 2 1 3 4 1 6 2 4 3 4 2 1 4 5 2 1 3 4 
 
 Page not found in buffer (miss)
-Page 1 placed at location 0
-Replacing frame at the oldest location (1) with page 1
+Page 3 placed at location 0
+Replacing oldest frame in buffer at location (0) with page 3
 Contents of Frame Buffer : 
-1
+3
 
 
 Page not found in buffer (miss)
-Page 3 placed at location 1
-Replacing frame at the oldest location (2) with page 3
+Page 2 placed at location 0
+Replacing oldest frame in buffer at location (1) with page 2
 Contents of Frame Buffer : 
+2
 3
-1
 
 
-The page 0 is found in the buffer (hit)
-
-Replacing page at index 2 in buffer 
-Replacing frame at the oldest location (2) with page 0
+Page not found in buffer (miss)
+Page 1 placed at location 1
+Replacing oldest frame in buffer at location (2) with page 1
 Contents of Frame Buffer : 
-0
-3
 1
+2
+3
 
 
 The page 3 is found in the buffer (hit)
 
 Replacing page at index 3 in buffer 
-Replacing frame at the oldest location (2) with page 3
 Contents of Frame Buffer : 
-0
-3
 1
+2
+3
 
 
 Page not found in buffer (miss)
-Page 5 placed at location 2
-Replacing frame at the oldest location (0) with page 5
+Page 4 placed at location 2
+Replacing oldest frame in buffer at location (1) with page 4
 Contents of Frame Buffer : 
-5
-3
 1
+4
+3
+
+
+The page 1 is found in the buffer (hit)
+
+Replacing page at index 5 in buffer 
+Contents of Frame Buffer : 
+1
+4
+3
 
 
 Page not found in buffer (miss)
-Page 6 placed at location 0
-Replacing frame at the oldest location (1) with page 6
+Page 6 placed at location 1
+Replacing oldest frame in buffer at location (0) with page 6
 Contents of Frame Buffer : 
-5
-3
+1
+4
 6
 
 
-Hit Ratio : 0.333333
-Miss Ratio: 0.666667
+Page not found in buffer (miss)
+Page 2 placed at location 0
+Replacing oldest frame in buffer at location (1) with page 2
+Contents of Frame Buffer : 
+1
+2
+6
+
+
+Page not found in buffer (miss)
+Page 4 placed at location 1
+Replacing oldest frame in buffer at location (2) with page 4
+Contents of Frame Buffer : 
+4
+2
+6
+
+
+Page not found in buffer (miss)
+Page 3 placed at location 2
+Replacing oldest frame in buffer at location (0) with page 3
+Contents of Frame Buffer : 
+4
+2
+3
+
+
+The page 4 is found in the buffer (hit)
+
+Replacing page at index 10 in buffer 
+Contents of Frame Buffer : 
+4
+2
+3
+
+
+The page 2 is found in the buffer (hit)
+
+Replacing page at index 11 in buffer 
+Contents of Frame Buffer : 
+4
+2
+3
+
+
+Page not found in buffer (miss)
+Page 1 placed at location 0
+Replacing oldest frame in buffer at location (0) with page 1
+Contents of Frame Buffer : 
+4
+2
+1
+
+
+The page 4 is found in the buffer (hit)
+
+Replacing page at index 13 in buffer 
+Contents of Frame Buffer : 
+4
+2
+1
+
+
+Page not found in buffer (miss)
+Page 5 placed at location 0
+Replacing oldest frame in buffer at location (1) with page 5
+Contents of Frame Buffer : 
+4
+5
+1
+
+
+Page not found in buffer (miss)
+Page 2 placed at location 1
+Replacing oldest frame in buffer at location (0) with page 2
+Contents of Frame Buffer : 
+4
+5
+2
+
+
+Page not found in buffer (miss)
+Page 1 placed at location 0
+Replacing oldest frame in buffer at location (2) with page 1
+Contents of Frame Buffer : 
+1
+5
+2
+
+
+Page not found in buffer (miss)
+Page 3 placed at location 2
+Replacing oldest frame in buffer at location (1) with page 3
+Contents of Frame Buffer : 
+1
+3
+2
+
+
+Page not found in buffer (miss)
+Page 4 placed at location 1
+Replacing oldest frame in buffer at location (0) with page 4
+Contents of Frame Buffer : 
+1
+3
+4
+
+
+Hit Ratio : 0.263158
+Miss Ratio: 0.736842
+
 
  */
